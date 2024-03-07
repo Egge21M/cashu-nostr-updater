57ec1e5f0e013a0ca83a1625889bb724bd6f2c84
Egge <egge@getcurrent.io>
2024-03-07 12:56:23 +0100

    making the linter happy

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,28 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+declare global {
+  interface Window {
+    nostr: {
+      getPublicKey: () => Promise<string>;
+      signEvent: (e: {
+        kind: number;
+        content: string;
+        created_at: number;
+        tags: string[][];
+      }) => Promise<{
+        kind: number;
+        content: string;
+        created_at: number;
+        tags: string[][];
+        id: string;
+        sig: string;
+        pubkey: string;
+      }>;
+    };
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
